0ef51d52d6d6307d815d44d168b41ad64217504c
Maciej Mycielski <maciejmycielski@gmail.com>
2020-10-25 11:00:17 +0100

    Models for lessons

--- services/classes.py
+++ services/classes.py
@@ -1,4 +1,5 @@
-from py2neo.ogm import GraphObject, Property
+from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
+from enum import Enum
 
 
 class User(GraphObject):
@@ -9,3 +10,55 @@ class User(GraphObject):
     is_teacher = Property()
     password = Property()
     hashed_password = Property()
+
+    lessons_taught = RelatedFrom("Lesson", "IS_TAUGHT_BY")
+    lessons_own = RelatedFrom("Lesson", "IS_OWNED_BY")
+
+
+class StudiesType(GraphObject):
+    __primarylabel__ = "studies_type"
+    __primarykey__ = "abbreviation"
+    name = Property()
+    abbreviation = Property()
+    type = Property()
+
+    lessons = RelatedFrom("Lesson", "IS_CONDUCTED_FOR")
+
+
+class TypeEnum(Enum):
+    SSI = 1
+    NSI = 2
+    SSM = 3
+    NSM = 4
+
+
+class FrequencyEnum(Enum):
+    Jednorazowe = 1
+    Tygodniowo = 2
+    Parzyste = 3
+    Nieparzyste = 4
+
+
+class LessonType(GraphObject):
+    __primarylabel__ = "lesson_type"
+    __primarykey__ = "name"
+    name = Property()
+    color = Property()
+
+    lessons = RelatedFrom("Lesson", "IS_TYPE")
+
+
+class Lesson(GraphObject):
+    __primarylabel__ = "lesson"
+    name = Property()
+    frequency = Property()
+    section = Property()
+    group = Property()
+    studies_type = Property()
+    duration = Property()
+
+    lesson_type = RelatedTo("LessonType")
+    teacher = RelatedTo("User")
+    owner = RelatedTo("User")
+    studies_type = RelatedTo("StudiesType")
+
